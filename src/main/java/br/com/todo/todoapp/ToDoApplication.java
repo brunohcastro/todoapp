debf3a4c17ca67cae2aba91de008b8e149f5d999
Bruno Henrique de Castro <brunohcastro@hotmail.com>
2019-04-14 04:25:59 -0300

    Change JSF implementation to Mojarra

--- src/main/java/br/com/todo/todoapp/ToDoApplication.java
+++ src/main/java/br/com/todo/todoapp/ToDoApplication.java
@@ -1,40 +1,41 @@
 package br.com.todo.todoapp;
 
-import org.apache.myfaces.webapp.StartupServletContextListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.Ordered;
+import org.springframework.web.context.ServletContextAware;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.faces.webapp.FacesServlet;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletContextListener;
 
 @SpringBootApplication
-public class ToDoApplication implements WebMvcConfigurer {
+public class ToDoApplication implements WebMvcConfigurer, ServletContextAware {
 
     public static void main(String[] args) {
         SpringApplication.run(ToDoApplication.class, args);
     }
 
     @Bean
-    public ServletRegistrationBean facesServletRegistration() {
-        ServletRegistrationBean registration = new ServletRegistrationBean(new FacesServlet(), "*.jsf");
+    public ServletRegistrationBean<FacesServlet> facesServletRegistration() {
+        ServletRegistrationBean<FacesServlet> registration = new ServletRegistrationBean<>(new FacesServlet(), "*.jsf");
         registration.setName("Faces Servlet");
+        registration.setLoadOnStartup(1);
         return registration;
     }
 
-    @Bean
-    public ServletContextListener startupServerContextListener() {
-        return new StartupServletContextListener();
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("forward:/index.jsf");
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
     }
 
     @Override
-    public void addViewControllers( ViewControllerRegistry registry ) {
-        registry.addViewController( "/" ).setViewName( "forward:/index.jsf" );
-        registry.setOrder( Ordered.HIGHEST_PRECEDENCE );
+    public void setServletContext(ServletContext servletContext) {
+        servletContext.setInitParameter("com.sun.faces.forceLoadConfiguration", Boolean.TRUE.toString());
     }
-
 }
